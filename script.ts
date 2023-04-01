8ea0966cc8f3df55e72aed27f7c5cc4c601ce76c
Saboor Hakimi <44523544+Saboor-Hakimi@users.noreply.github.com>
2023-04-01 14:50:09 +0430

    create user which owns a post, and retriving it

--- script.ts
+++ script.ts
@@ -2,14 +2,37 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+    // const user = await prisma.user.create({
+    //     data: {
+    //         name: "Saboor",
+    //         email: "saboor@google.com",
+    //         password: "123456",
+    //         posts: {
+    //             create: {
+    //                 title: "Hello World",
+    //                 content: "This is my first post",
+    //             },
+    //         },
+    //     },
+    // });
+
+    // console.log(user)
+
+    const users = await prisma.user.findMany({
+        include: {
+            posts: true,
+        },
+    });
+    console.dir(users, { depth: null });
+}
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
